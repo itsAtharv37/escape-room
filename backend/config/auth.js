b14d9856ca46c23528082e7231939fb44f77c824
AaTherf <it.atharv.m39@gmail.com>
2023-10-18 17:34:39 +0530

    [NOT TESTED] user auth setup

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,1 +1,56 @@
-// jwt auth
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+
+const secretKey = process.env.authKey
+
+const User = require('../models/user')
+
+function authToken(req,res,next){
+    const token = req.header('Authorizaion')
+    if(!token){
+        return res.status(401).send('No token provided.')
+    }
+
+    jwt.verify(token, secretKey, (err,user) => {
+        if(err){
+            return res.status(403).send("Your token is invalid")
+        }
+
+        req.user = user
+        next()
+    })
+}
+
+router.get('/protected-route', authToken, (res) => {
+    res.json({ message: 'protected route'})
+})
+
+router.post('/login', async (req,res) => {
+    const{username,password} = req.body
+
+    try{
+        const user = await User.findOne({username})
+        if(!user){
+            return res.status(401).json({message: "Invalid username"})
+        }
+
+        const passMatch = await bcrypt.compare(password,user.password)
+        if(passMatch){
+            const token = jwt.sign({
+                id: user._id,
+                username: user.username
+            }, secretKey)
+
+            res.json({token})
+        }else{
+            res.status(401).json({message: 'Invalid password'})
+        }
+    } catch(error){
+        console.error(error)
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
+module.exports = router
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const router = express.Router()
-
-const gameController = require('../controllers/gameController')
+const User = require('../models/user')
 const userController = require('../controllers/userController')
 
-router.post('/login', userController.login)
-router.post('/register' , userController.register)
+// const gameController = require('../controllers/gameController')
+// const userController = require('../controllers/userController')
+
+// router.post('/login', userController.login)
+// router.post('/register' , userController.register)
+
+router.get('/users', async(req,res) => { // users list
+    try{
+        const users = await User.find()
+        res.json(users)
+    }catch(error){
+        console.error(500).json({message: 'Server error'})
+    }
+})
+
+router.post('/users', async (req,res) => { // new user
+    try{
+        const{username,email,pasword} = req.body
+        const newUser = new User({username,email,password})
+        await newUser.save()
+    } catch(error){
+        console.error(error)
+        res.status(500).json({message: 'server error'})
+    }
+})
+
+router.get('/users/:username', async(req,res) => {
+    try{
+        const username = req.params.username
+
+        const user = await User.findOne({username})
+        if(user){
+            res.json(user)
+        } else{
+            res.status(404).json({message: "User not found"})
+        }
+    } catch(error){
+        console.error(error)
+        res.status(500).json({message: 'Server errpr'})
+    }
+})
+
+router.post('/login',userController.login)
+router.post('/register',userController.register)
 
 module.exports = router
